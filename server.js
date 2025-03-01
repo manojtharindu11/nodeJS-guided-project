6ccad0f6d97beab7c39f5b2a66ae7baf9bcab957
Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2025-03-01 20:28:06 +0530

    add API endpoint to create new products in the database

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var db = require("./database.js");
 var bodyParser = require("body-parser");
 const { request, response } = require("express");
+const send = require("send");
 
 var app = express();
 
@@ -11,4 +12,71 @@ let HTTP_PORT = 8080;
 
 app.listen(HTTP_PORT, () => {
   console.log(`Server is running on ${HTTP_PORT}`);
+});
+
+app.post("/api/products", (req, res, next) => {
+  try {
+    var errors = [];
+
+    if (!req.body) {
+      errors.push("An invalid input");
+    }
+
+    const {
+      productName,
+      description,
+      category,
+      brand,
+      expireData,
+      manufacturedData,
+      batchNumber,
+      unitPrice,
+      quantity,
+      createdDate,
+    } = req.body;
+
+    var sql = `
+            INSERT INTO products (
+                productName,
+                description,
+                category,
+                brand,
+                expireData,
+                manufacturedData,
+                batchNumber,
+                unitPrice,
+                quantity,
+                createdDate
+            ) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+          `;
+    var params = [
+      productName,
+      description,
+      category,
+      brand,
+      expireData,
+      manufacturedData,
+      batchNumber,
+      unitPrice,
+      quantity,
+      createdDate,
+    ];
+
+    db.run(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      } else {
+        res.json({
+          message: "Success",
+          data: res.body,
+          id: this.lastID,
+        });
+      }
+    });
+  } catch (err) {
+    res.status(400);
+    send(err);
+  }
 });
